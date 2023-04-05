bb0326488888a241cac3cf292270a682e9b9949d
studiobvio <will@studiobvio.com>
2023-04-05 16:04:33 -0600

    Building Template Functions and md dowload

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,31 +1,102 @@
 function generateMarkdown() {
   // get values from form inputs
-  var title = document.getElementById("title").value;
-  var author = document.getElementById("author").value;
-  var date = document.getElementById("date").value;
-  var category = document.getElementById("category").value;
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const date = document.getElementById("date").value;
+  const category = document.getElementById("category").value;
+  const tags = document.getElementById("tags").value.split(",");
 
-  // create YAML front matter string
-  var frontMatter = "---\n";
-  frontMatter += "title: " + title + "\n";
-  frontMatter += "author: " + author + "\n";
-  frontMatter += "date: " + date + "\n";
-  frontMatter += "category: " + category + "\n";
-  frontMatter += "---\n";
+  // build front matter string with YAML syntax
+  let frontMatter = `---
+title: ${title}
+author: ${author}
+date: ${date}
+category: ${category}\n`;
 
-  // create markdown template
-  var markdownTemplate = "<!-- Enter your content here -->";
+  // add tags to front matter string
+  if (tags.length > 0) {
+    frontMatter += "tags:\n";
+    tags.forEach((tag) => {
+      frontMatter += `  - "${tag.trim()}"\n`;
+    });
+  }
 
-  // create markdown with front matter and template
-  var markdown = frontMatter + "\n" + markdownTemplate + "\n";
+  // close front matter section
+  frontMatter += `---\n\n`;
 
-  // display markdown in code block
-  var codeBlock = document.getElementById("markdown-code");
-  codeBlock.textContent = markdown;
+  // build markdown template string
+  const template = `<--your context here -->`;
+
+  // combine front matter and template strings into full markdown string
+  const fullMarkdown = `${frontMatter}${template}`;
+  document.getElementById("markdown-code").textContent = fullMarkdown;
+
+  // show code display area
+  document.getElementById("code-display").classList.remove("d-none");
+}
+
+function copyToClipboard() {
+  const codeElement = document.getElementById("markdown-code");
+  const text = codeElement.textContent;
+  navigator.clipboard
+    .writeText(text)
+    .then(() => {
+      console.log("Code copied to clipboard.");
+    })
+    .catch((error) => {
+      console.error(`Error copying code to clipboard: ${error}`);
+    });
+}
+
+function downloadMarkdown() {
+  // get values from form inputs
+  const title = document.getElementById("title").value;
+
+  // build full markdown string
+  const template = `<--your context here -->`;
+  const frontMatter = `---
+    title: ${title}
+    `;
+  const fullMarkdown = `${frontMatter}${template}`;
+}
+
+function downloadMarkdown() {
+  // get values from form inputs
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const date = document.getElementById("date").value;
+  const category = document.getElementById("category").value;
+  const tags = document.getElementById("tags").value.split(",");
+
+  // build front matter string with YAML syntax
+  let frontMatter = `---
+title: ${title}
+author: ${author}
+date: ${date}
+category: ${category}\n`;
+
+  // add tags to front matter string
+  if (tags.length > 0) {
+    frontMatter += "tags:\n";
+    tags.forEach((tag) => {
+      frontMatter += `  - "${tag.trim()}"\n`;
+    });
+  }
+
+  // close front matter section
+  frontMatter += `---\n\n`;
+
+  // build markdown template string
+  const template = `<--your context here -->`;
+
+  // combine front matter and template strings into full markdown string
+  const fullMarkdown = `${frontMatter}${template}`;
+  // create a Blob object with the full markdown string
+  const markdownBlob = new Blob([fullMarkdown], { type: "text/markdown" });
 
   // download markdown file
   var fileName = title.toLowerCase().replace(/ /g, "-") + ".md";
-  var blob = new Blob([markdown], { type: "text/markdown" });
+  var blob = new Blob([markdownBlob], { type: "text/markdown" });
   var link = document.createElement("a");
   link.href = URL.createObjectURL(blob);
   link.download = fileName;
